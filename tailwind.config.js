573de7f18cdafdb8dd8e9d6e675319d5bad515b8
David R. Hanson <drh@drhanson.net>
2024-10-25 16:46:15 -0600

    Change breakpoint for "xl" to 1080px.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
             'serif': [ 'Times', 'Times New Roman', 'ui-serif', 'Georgia', 'Cambria', 'serif' ],
             'sans': [ 'ui-sans-serif', 'system-ui', 'Arial', 'sans-serif' ],
         },
+        screens: {
+            'xl': '1080px',
+            // => @media (min-width: 1080px) { ... }
+        },
     },
     extend: {},
     plugins: [],
