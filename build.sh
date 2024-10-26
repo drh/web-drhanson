244d0b52b8bca3ef167441be7c86775fd04234d9
David R. Hanson <drh@drhanson.net>
2024-10-26 14:02:11 -0600

    Revise build.sh to fetch tailwind CSS and run it to generate assets/style.css before running hugo. Delete assets/style.css.

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
+# Build drhanson.net using tailwind CSS + Hugo
+
+# Fetch tailwind CSS
+curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-arm64
+chmod +x tailwindcss-linux-arm64
+mv tailwindcss-linux-arm64 tailwindcss
+
+# Generate style.css and run Hugo
+./tailwindcss -i assets/input.css -o assets/style.css
 hugo build
