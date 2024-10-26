b77f9a1821dba172da21b0251b320bbb2591bb28
David R. Hanson <drh@drhanson.net>
2024-10-26 14:25:47 -0600

    Fixed botched architecture type. Show commands executed.

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 # Build drhanson.net using tailwind CSS + Hugo
 
 # Fetch tailwind CSS
-curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-arm64
-chmod +x tailwindcss-linux-arm64
-mv tailwindcss-linux-arm64 tailwindcss
+set -o xtrace
+curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
+chmod +x tailwindcss-linux-x64
+mv tailwindcss-linux-x64 tailwindcss
 
 # Generate style.css and run Hugo
 ./tailwindcss -i assets/input.css -o assets/style.css
+set +o xtrace
 hugo build
