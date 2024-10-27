3d190cd81256bfbffa044b80e884da6193e35441
David R. Hanson <drh@drhanson.net>
2024-10-27 11:53:00 -0600

    Use npm to build Tailwind CSS.

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
-# Build using tailwind CSS + Hugo
+# Build using Tailwind CSS + Hugo
 
-# Fetch tailwind CSS
-curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
-chmod +x tailwindcss-linux-x64
-mv tailwindcss-linux-x64 tailwindcss
+# Install tailwind CSS
+npm install -D tailwindcss
 
 # Generate style.css and run Hugo
-./tailwindcss -i assets/input.css -o assets/style.css
+npx tailwindcss -i assets/input.css -o assets/style.css
 hugo build
